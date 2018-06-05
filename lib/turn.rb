c5ddb72ecd567bfff841e08c2a12d82e59f4dd16
Kristen Papke <klpapke215@gmail.com>
2018-06-05 02:28:19 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,6 +36,8 @@ def turn(board)
   index = input_to_index(input)
   if(valid_move?(board, index))
     move(board, index, character = "X")
+  elsif display_board(board)
+    
   else
       turn(board)
   end
